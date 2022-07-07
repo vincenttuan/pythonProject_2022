b1b41806b7819c25a7bf2c70a130f38e62e05bef
vincenttuan <vincentjava@yahoo.com.tw>
2022-07-07 10:03:33 +0800

    python

--- day3/For迴圈.py
+++ day3/For迴圈.py
@@ -4,12 +4,12 @@
 # 1. 使用 基礎迴圈 - while
 x = 1
 while x <= 10:  # 當 x <= 10 的時候執行
-    print(x, end=" ")
+    print(x, end=' ')  # end=' ' 表示結尾放一個空白
     x = x + 1
 print()
 # 2. 使用 步進迴圈 - for in
 # range(n, m) 範圍 n <= x < m
 for x in range(1, 11):  # 特別注意: 10 + 1 = 11
-    print(x, end=" ")
+    print(x, end=' ')
 
 
