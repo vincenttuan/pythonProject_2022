e4331e469957ded6743c6d91b45dd3187d9043ef
vincenttuan <vincentjava@yahoo.com.tw>
2022-07-07 16:22:15 +0800

    python

--- day4/樂透號碼電腦選號.py
+++ day4/樂透號碼電腦選號.py
@@ -7,6 +7,21 @@ for i in range(0, 4):
     print('n =', n)
     four_stars.append(n)
 print(four_stars, len(four_stars))
+
+print('----------------')
 # 四星彩(數字不可重複版)
 # 0~9 任意取出 4 個數字, 數字不可以重複
+four_stars = []
+while len(four_stars) < 4:
+    n = random.randint(0, 9)
+    print('n =', n)
+    try:
+        # n 是否在 four_stars 中
+        # 若 n 沒有在 four_stars 中則會拋出例外
+        four_stars.index(n)
+    except ValueError as ve:
+        four_stars.append(n)
+
+print(four_stars, len(four_stars))
+
 
