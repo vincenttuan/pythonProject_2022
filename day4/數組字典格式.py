2ca00b8f6f085794b03d411325c41ec11697df88
vincenttuan <vincentjava@yahoo.com.tw>
2022-07-07 16:39:18 +0800

    python

---
+++ day4/數組字典格式.py
@@ -0,0 +1,7 @@
+# dict 字典格式 (key-value)
+emp1 = {'name': 'John', 'salary': 55000}
+emp2 = {'name': 'Mary', 'salary': 78000}
+emp3 = {'name': 'Bob', 'salary': 61000}
+# 將 emp1, emp2, emp3 放入 employees 數組[]中
+employees = [emp1, emp2, emp3]
+print(employees)
