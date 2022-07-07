e6341a764bc0742e48929743bc010ce2c88acde0
vincenttuan <vincentjava@yahoo.com.tw>
2022-07-07 10:50:06 +0800

    python

---
+++ day3/For迴圈4.py
@@ -0,0 +1,12 @@
+# 隨堂練習
+# 試印出
+# *
+# **
+# ***
+# ****
+# *****
+max_value = 5
+for x in range(0, max_value):  # 0..max_value-1
+    for y in range(0, x+1):
+        print('*', end=' ')
+    print()
