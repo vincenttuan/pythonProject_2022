19cf52df28acf119fa95222737eb3bddf6a0efc0
vincenttuan <vincentjava@yahoo.com.tw>
2022-07-07 15:59:10 +0800

    python

--- day4/撲克牌2.py
+++ day4/撲克牌2.py
@@ -3,8 +3,13 @@ import random
 poker = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
 #         0   1  2  3  4  5  6  7  8   9   10   11   12
 print(poker, len(poker))
-# 隨機洗牌
-x = random.randint(0, len(poker)-1)
-y = random.randint(0, len(poker)-1)
-poker[x], poker[y] = poker[y], poker[x]
+# for-in + 隨機洗牌
+for i in range(0, 100):
+    x = random.randint(0, len(poker)-1)
+    y = random.randint(0, len(poker)-1)
+    poker[x], poker[y] = poker[y], poker[x]
+print(poker)
+
+# 透過 random.shuffle() 來隨機洗牌
+random.shuffle(poker)
 print(poker)
