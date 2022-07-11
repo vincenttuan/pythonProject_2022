77dc0421391c23a290fb7f1fc380f8ff4ae165f1
vincenttuan <vincentjava@yahoo.com.tw>
2022-07-11 14:55:45 +0800

    python

--- day6/lambda_demo6.py
+++ day6/lambda_demo6.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 # 1. lambda parameter_list: expression
 max_func = lambda a, b: a if a > b else b
 max_value = max_func(10, 20)
@@ -24,13 +26,15 @@ scores = [50, 80, 90, 30]  # [0, 1, 1, 0]
 result = map(lambda x: 1 if x >= 60 else 0, scores)
 print(list(result))
 
-
-
-
-
-
 # 5. reduce(lambda param1, param2: expression, iterable)
 # reduce 歸納
+scores = [50, 80, 90, 30]
+result = reduce(lambda x, y: x + y, scores)
+# [50, 80, 90, 30]
+# 第一次歸納: x=50, y=80: 130 <-- 下一次歸納的 x
+# 第二次歸納: x=130, y=90: 220 <-- 下一次歸納的 x
+# 第三次歸納: x=220, y=30: 250 <-- 歸納結束
+print(result)
 
 
 # 6. sorted(iterable, key=lambda parameter_list: expression)
