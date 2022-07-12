ef4ff2d0168e0e5d05ce75477c9a9c03c72f4304
vincenttuan <vincentjava@yahoo.com.tw>
2022-07-12 12:27:40 +0800

    python

--- day8/oo_5.py
+++ day8/oo_5.py
@@ -6,18 +6,19 @@ import math
 class User:
     height = 0
     weight = 0
-    bmi = 0.0
+    __bmi = 0.0  # __私有變數
 
     def __init__(self, height, weight):
         self.height = height
         self.weight = weight
-        self.bmi = weight / math.pow(height/100, 2)
+        self.__bmi = weight / math.pow(height/100, 2)
 
     def __str__(self):
-        return 'height: %d weight: %d bmi: %.1f' % (self.height, self.weight, self.bmi)
+        return 'height: %d weight: %d bmi: %.1f' % (self.height, self.weight, self.__bmi)
 
 
 if __name__ == '__main__':
     user1 = User(170, 60)
+    user1.__bmi = 0  # 無法改變物件私有變數
     print(user1)
 
