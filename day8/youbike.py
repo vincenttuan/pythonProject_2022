be451d3672da51809f214db3613f00cdd585cc3c
vincenttuan <vincentjava@yahoo.com.tw>
2022-07-12 16:46:29 +0800

    python

--- day8/youbike.py
+++ day8/youbike.py
@@ -18,3 +18,19 @@ class Youbike:
         return '站點: %s 站名: %s 可借: %d 可還 %d 全部: %d 緯度: %f 經度: %f' % \
                (self.sno, self.sna, self.sbi, self.bemp, self.tot, self.lat, self.lng)
 
+
+def import_data_to_youbike():
+    url = "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
+    data_json = json.loads(requests.get(url).text)
+    youbikes = []
+    for data in data_json:
+        youbike = Youbike(data['sno'], data['sna'], data['sbi'], data['bemp'], data['tot'], data['lat'], data['lng'])
+        youbikes.append(youbike)
+    return youbikes
+
+
+if __name__ == '__main__':
+    youbikes = import_data_to_youbike()
+    print('筆數: %d' % len(youbikes))
+    for youbike in youbikes:
+        print(youbike)
