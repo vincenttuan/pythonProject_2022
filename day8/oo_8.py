33559bbb4cb499acf612eae8c16c5114b65d9297
vincenttuan <vincentjava@yahoo.com.tw>
2022-07-12 15:09:00 +0800

    python

--- day8/oo_8.py
+++ day8/oo_8.py
@@ -2,22 +2,28 @@
 class Employee:  # 員工
     def __init__(self, title):
         self.title = title  # 職銜
+
     def __str__(self):
         return "Employee's title: %s " % self.title
+
 
 class Computer:  # 電腦
     def __init__(self, cpu_speed):
         self.cpu_speed = cpu_speed
+
     def __str__(self):
         return "Computer's cpu speed: %.1f Ghz " % self.cpu_speed
+
 
 class Robot(Employee, Computer):  # 機器人繼承員工,電腦
     def __init__(self, title, cpu_speed, name):
         Employee.__init__(self, title)
         Computer.__init__(self, cpu_speed)
         self.name = name
+
     def __str__(self):
         return Employee.__str__(self) + Computer.__str__(self) + 'name: %s' % self.name
+
 
 if __name__ == '__main__':
     robot = Robot('保全', 3.1, '門禁機器人')
