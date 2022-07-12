22adaf049ec8aa32f6af53bd388d3ff6d15d3f01
vincenttuan <vincentjava@yahoo.com.tw>
2022-07-12 11:00:46 +0800

    python

--- day7/食品安全爬蟲分析.py
+++ day7/食品安全爬蟲分析.py
@@ -12,10 +12,22 @@ def load_json_data():  # 將資料載入到 json 物件中
 
 def data_analyzing(name):  # 根據 name(品名) 的內容進行搜尋
     json_data = load_json_data()
+    bad_food = []
     for data in json_data:
         if name in data['品名']:  # 關鍵字比對
-            print('品名: %s 不合格原因: %s' % (data['品名'], data['不合格原因']))
+            bad_food.append('品名: %s 不合格原因: %s' % (data['品名'], data['不合格原因']))
+    return bad_food
+
+
+def write_to_file(data, file_name):  # 將結果寫入到檔案
+    file = open(file_name, 'w', encoding='UTF-8')
+    for row in data:
+        file.write(row + '\n')
+    file.close()
 
 
 if __name__ == '__main__':
-    data_analyzing('池上')
+    bad_food = data_analyzing('池上')
+    print(bad_food)
+    write_to_file(bad_food, 'bad_food.txt')
+    print('寫檔完畢')
