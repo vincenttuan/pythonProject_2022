8b31787e97aacce5a9bfef7cda4af56be55ed662
vincenttuan <vincentjava@yahoo.com.tw>
2022-07-12 10:15:50 +0800

    python

--- day7/read_from_file.py
+++ day7/read_from_file.py
@@ -5,8 +5,8 @@ file = open('salary.txt', 'r', encoding='UTF-8')
 rows = file.readlines()
 print(type(rows), rows)
 # 進行資料整理
-# 將 list: ['John,45000\n', 'Mary,55000\n', 'Tom,38000\n', 'Helen,65000\n', 'Boss,150000\n']
-# 變 dict: [{'name': 'John', 'salary': 45000}, {'name': 'Mary', 'salary': 55000}, ...]
+# 將 list<str> : ['John,45000\n', 'Mary,55000\n', 'Tom,38000\n', 'Helen,65000\n', 'Boss,150000\n']
+# 變 list<dict>: [{'name': 'John', 'salary': 45000}, {'name': 'Mary', 'salary': 55000}, ...]
 employees = []  # 建立一個空的數組(容器)
 for row in rows:
     row = row.strip('\n')  # 去除 \n
