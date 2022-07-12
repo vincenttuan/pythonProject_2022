a155996d770a230e1b1f479b2f80bc5bcee379f7
vincenttuan <vincentjava@yahoo.com.tw>
2022-07-12 14:17:14 +0800

    python

--- day8/oo_6.py
+++ day8/oo_6.py
@@ -8,7 +8,16 @@ class Account:
     def __str__(self) -> str:
         return '%s 的帳戶餘額 $%d' % (self.name, self.__balance)
 
+    def deposit(self, amount):  # 存款
+        if amount > 0:
+            self.__balance += amount  # self.__balance = self.__balance + amount
+            return True, '存款成功'
+        else:
+            return False, '存款失敗。存款金額錯誤: $%d' % amount
+
 
 if __name__ == '__main__':
     act1 = Account('John')  # 開戶
+    success, msg = act1.deposit(1000)
+    print(success, msg)
     print(act1)
