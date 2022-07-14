6e6680ced9ed486e67d262087f89e9845248c5fc
vincenttuan <vincentjava@yahoo.com.tw>
2022-07-14 15:12:48 +0800

    python

--- day11/gui_4.py
+++ day11/gui_4.py
@@ -8,10 +8,22 @@ import tkinter
 # |   Exit   |
 # +----------+
 
+
+def win_exit():
+    win.quit()
+
+
 if __name__ == '__main__':
     win = tkinter.Tk()
     win.title('My Clock')
-    win.geometry('300x300')
-    data = tkinter.StringVar  # 建立一個 tkinter 的可變字串
+    win.geometry('500x250')
+
+    data = tkinter.StringVar()  # 建立一個 tkinter 的可變字串
+    data.set('00:00:00')  # 設定 data 的內容
     time_label = tkinter.Label(win, textvariable=data, fg='blue', font=('Arial', 80))
+    time_label.pack()
+
+    exit_button = tkinter.Button(win, text='Exit', font=('Arial', 80), command=win_exit)
+    exit_button.pack()
+
     win.mainloop()
