b6a97305faa2123f9db8c4cb6dcfe18ff65bbdcf
vincenttuan <vincentjava@yahoo.com.tw>
2022-07-14 10:51:51 +0800

    python

---
+++ day9/exception_demo4.py
@@ -0,0 +1,16 @@
+# 讀寫一個檔案
+# 1. 開啟檔案, 2. 讀寫入資料
+# r+: opens the file for both reading and writing
+# w+: opens for writing (and thus truncates the file) and reading
+# r: reading
+# w:writing
+try:
+    f = open('demo.txt', 'r+', encoding="UTF-8")
+    print(f.readlines())
+    try:
+        f.write("\nPython AI")
+    except Exception as e:
+        print('資料寫入失敗!', e)
+except FileNotFoundError as fnf:
+    print('檔案找不到:', fnf)
+
