6adae89401de1a80adfe7754a5bc795d6b04f686
vincenttuan <vincentjava@yahoo.com.tw>
2022-07-14 10:11:51 +0800

    python

---
+++ day9/exception_demo2.py
@@ -0,0 +1,25 @@
+apple = 10  # 蘋果數量
+nums = [2, 0, 5]  # 人數
+#       0  1  2   # 位置
+print('蘋果有 %d 個' % apple)
+# -------------------------------------------------
+# 利用錯誤處理驗證
+try:
+    idx = int(input('請輸入人數位置 0=2, 1=0, 2=5: '))
+except ValueError as ve:
+    print('輸入了非數字資料:', ve)
+    exit()  # 離開程式
+
+# 利用程式邏輯驗證
+if idx < 0 or idx > len(nums)-1:
+    print('超過 nums 位置(index)可選範圍')
+    exit()
+
+if nums[idx] == 0:
+    print('分母不可為0')
+    exit()
+# -------------------------------------------------
+# 正式進入計算程序
+print('有 %d 人分' % nums[idx])
+avg = apple / nums[idx]
+print('每個人可以得到 %d 個蘋果' % avg)
